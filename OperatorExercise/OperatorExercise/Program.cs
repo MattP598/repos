e13708de12a6aa3e6d46baf73e418d8d6343cd79
Matt <MattP598@gmail.com>
2022-04-26 09:28:27 -0500

    Operators Exercise

---
+++ OperatorExercise/OperatorExercise/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace OperatorExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 17;
+            int b = 4;
+            int quotient = a / b;
+            int remainder = a % b;
+
+            if (a == 17 && b == 4)
+            {
+                Console.WriteLine("{0}/{1} is {3} remainder {4}.", a, b, quotient, remainder);
+            }
+        }
+    }
+}
