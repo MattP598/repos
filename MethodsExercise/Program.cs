56a25c0f4c159182cbb365e7c653f4536c65e7d2
Matt <MattP598@gmail.com>
2022-04-26 03:06:10 -0500

    Methods Exercise

---
+++ MethodsExercise/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace MethodsExercise
+{
+    internal class Program
+    {
+        public static int Subtract(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+
+        public static int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public static int Multiply(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+
+        public static int Divide(int num1, int num2)
+        {
+            return num1 / num2;
+        }
+
+
+        static void Main(string[] args)
+        {
+            int num1;
+            int num2;
+            void 
+            
+            Console.WriteLine("Please enter first number: ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please enter second number: ");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("The sum of the two numbers is " +Sum);
+        }
+    }
+}
