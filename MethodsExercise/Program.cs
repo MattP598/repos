f5e787eee064ead134b7d5abc4b0c3fc65fc31b8
Matt <MattP598@gmail.com>
2022-04-26 08:57:19 -0500

    Methods Exercise

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -2,15 +2,20 @@
 
 namespace MethodsExercise
 {
-    internal class Program
+    public class Exercises
     {
-        public static int Subtract(int num1, int num2)
+        public static int GetNumbers()
         {
+            Console.WriteLine("Please enter the first number: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter the second number: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
             return num1 - num2;
         }
 
         public static int Add(int num1, int num2)
         {
+            
             return num1 + num2;
         }
 
@@ -23,21 +28,3 @@ namespace MethodsExercise
         {
             return num1 / num2;
         }
-
-
-        static void Main(string[] args)
-        {
-            int num1;
-            int num2;
-            void 
-            
-            Console.WriteLine("Please enter first number: ");
-            num1 = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("Please enter second number: ");
-            num2 = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("The sum of the two numbers is " +Sum);
-        }
-    }
-}
